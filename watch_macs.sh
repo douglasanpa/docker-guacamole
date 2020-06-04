2ae9dd5ea2bddf860542398319946b314714aa4e
Douglas Andrade <douglasanpa@msn.com>
2020-06-04 20:46:47 -0300

    Update watch_macs.sh

--- watch_macs.sh
+++ watch_macs.sh
@@ -1,4 +1,4 @@
 #!/bin/bash
-
-echo "$WOL_ID" | tr '\' '\n' | sed -r 's/[ ]+//g' > /etc/macs.list
+echo "id,mac" > /etc/macs.list
+echo "$WOL_ID" | tr '\' '\n' | sed -r 's/[ ]+//g' >> /etc/macs.list
 /usr/local/bin/guac_wol.py &
